391eceb823e3d216a0d5fb336c7df01e99bd1004
iniubongisemin <eugeneinie@gmail.com>
2024-01-25 16:02:01 +0100

    feat: updated the app

--- hitechapp/models.py
+++ hitechapp/models.py
@@ -9,12 +9,17 @@ class BlogPost(models.Model): # models is the file, Model is the class
     update_at = models.DateTimeField(auto_now=True)
     view_count = models.PositiveBigIntegerField()  
     # view_count = models.IntegerField()  
+    published = models.BooleanField(default=False)
+    rating = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self): # represents an object of the class
+        return self.title
 
     class Meta: # to specify a special database name for some feature
         db_table = 'post' # name in your database NB: checkout django docs for more info
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
         ordering = [-1] # this reverts the order
-        ordering = ['post_at'] # order by date
-        ordering = ['-post_at'] # reverts the order by date
+        ordering = ['posted_at'] # order by date
+        # ordering = ['-posted_at'] # reverts the order by date
 
